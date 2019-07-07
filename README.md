0fc901cc359a991b9999d958bb382bb0debcd2ae
apc <alexcurran.va@gmail.com>
2019-07-07 15:46:57 -0500

    Finished animation for keypresses and added a background image with filter effects

---
+++ README.md
@@ -0,0 +1,13 @@
+# JS Sound Machine
+
+[Link to Site](https://apcurran.github.io/sound-machine/)
+
+## Features:
+
+- JS for keypress and click events on buttons
+
+- Complete with animation for button pressing effects
+
+- Flexbox used for a responsive layout
+
+- Mobile-friendly
--- script.js
+++ script.js
@@ -7,13 +7,23 @@ const soundMachine = (() => {
             sounds[i].currentTime = 0;
             sounds[i].play();
         });
+
+        // Remove the animation class after transition has finished on the current key.
+        key.addEventListener("transitionend", (event) => {
+            const key = event.target;
+            key.classList.remove("active");
+        });
     });
 
     window.addEventListener("keydown", (event) => {
         const currentSoundSelection = document.querySelector(`audio[data-keycode="${event.keyCode}"]`);
+        const currentKey = document.querySelector(`div[data-keycode="${event.keyCode}"]`);
     
         currentSoundSelection.currentTime = 0;
         currentSoundSelection.play();
+
+        // Activate active animation for the current key.
+        currentKey.classList.add("active");
     });
 
-})(); // End of module
+})();
